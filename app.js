cffd176aced60a081e519947b04d266d3b372b13
unknown <molanddise@gmail.com>
2026-04-30 15:21:57 -0600

    The seed.js file was created to check the connection if there are users

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const { Pool } = require('pg');
 const { PrismaPg } = require('@prisma/adapter-pg');
-const { PrismaClient } = require('./generated/prisma');
+const { PrismaClient } = require('@prisma/client');
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
---
+++ prisma/seed.js
@@ -0,0 +1,33 @@
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { PrismaClient } = require('@prisma/client');
+
+require('dotenv').config();
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  // const users = [
+  //   { name: "User 1", email: "user1@example.com" },
+  //   { name: "User 2", email: "user2@example.com" },
+  //   { name: "User 3", email: "user3@example.com" },
+  // ];
+
+  // for (const user of users) {
+  //   await prisma.user.create({
+  //     data: user,
+  //   });
+  // }
+  // console.log("Demo user successfully created");
+
+  await prisma.user.deleteMany();
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+    await pool.end();
+  });
