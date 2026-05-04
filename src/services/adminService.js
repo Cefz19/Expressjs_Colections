f279afbbd7bd9c87aa8eb71c9fa15700f038ea6c
unknown <molanddise@gmail.com>
2026-05-04 15:17:37 -0600

    The admin all modules were created to have logic and can make petitions

---
+++ src/services/adminService.js
@@ -0,0 +1,28 @@
+const { Pool } = require("pg");
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { PrismaClient } = require("@prisma/client");
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+const createTimeBlockService = async (startTime, endTime) => {
+    const newTimeBlock = await prisma.timeBlock.create({
+        data: {
+            startTime: new Date(startTime),
+            endTime: new Date(endTime)
+        }
+    });
+    return newTimeBlock;
+} 
+
+const listReservationsService = async () => {
+    const reservations = await prisma.appointment.findMany({
+        include: {
+            user: true,
+            timeBlock: true
+        }
+    });
+    return reservations;
+}
+
+module.exports = { createTimeBlockService, listReservationsService }
